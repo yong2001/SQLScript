9c3e132f0a644b509b3740c6b34e378f463103b9
yong2001 <y-ma@sonorite.co.jp>
2022-03-10 10:37:50 +0900

    Create sharepointups.sql
    New

---
+++ sharepointups.sql
@@ -0,0 +1,6 @@
+SELECT
+RecordID,PropertyID,PropertyVal
+FROM
+[User Profile Service アプリケーション_ProfileDB].[dbo].[UserProfileValue]
+Where
+PropertyID = '3' OR PropertyID = '7' OR PropertyID = '9' OR PropertyID = '13' OR PropertyID = '14' OR PropertyID = '19' 
